7fc781f3f8f32ac83fa48d4630fc54ce92f94a62
Nataliia Tur <natalim@ukr.net>
2025-02-14 20:49:37 +0200

    homework 11

---
+++ src/js/render-functions.js
@@ -0,0 +1,42 @@
+'use strict';
+
+import axios from 'axios';
+import iziToast from 'izitoast';
+import "iziToast/dist/css/iziToast.min.css";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+export function renderImages(images, galleryList) {
+    const markup = images.map(img => 
+        `<li>
+            <div class="gallery">
+                <a href="${img.largeImageURL}">
+                    <img src="${img.largeImageURL}" alt="${img.tags}" title="${img.tags}"/>
+                </a>
+            </div>
+            <div class="info">
+                <p><b>Likes:</b> ${img.likes}</p>
+                <p><b>Views:</b> ${img.views}</p>
+                <p><b>Comments:</b> ${img.comments}</p>
+                <p><b>Downloads:</b> ${img.downloads}</p>
+            </div>
+        </li>`
+    ). join('');
+    
+    galleryList.insertAdjacentHTML('beforeend', markup);
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+        enableKeyboard: true, 
+        closeOnClick: true,   
+        swipeClose: true  
+    });
+
+    lightbox.refresh();
+}
+
+
+
